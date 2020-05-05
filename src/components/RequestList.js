b3cb17a8f6d3319a2837d13e1c74640f791c2370
mertnacakgedigi <mert.nacakgedigi@gmail.com>
2020-05-05 10:18:17 -0700

    getting request to homepage is working

--- src/components/RequestList.js
+++ src/components/RequestList.js
@@ -1,19 +1,71 @@
 import React, { Component } from 'react';
+import {Link , NavLink, Route} from 'react-router-dom'
+import RequestModel from '../models/request'
+import Table from 'react-bootstrap/Table'
+
 
 class RequestList extends Component {
 
+    state = {
+        requestList: [],
+        isLoaded: false,
+        requestId: '',
+    } 
    
-    render(props) {
-       let name = this.props.requestList
+    //We have access to this.props.currentUser
+    componentDidMount() {
+       RequestModel.index()
+       .then((res) => {
+          
+           this.setState({
+               requestList: res.data,
+               isLoaded: true,
+           })
+       })
+    }
 
-       console.log(name)
-       if(name[0]){
+    render() {
         return (
-            <div>
-                {/* {this.props.requestList[0].name}r */}
-            </div>
+                    <>
+
+                    <h1>Request List</h1>
+                    <Table striped bordered hover>
+                        <thead>
+                            <tr>
+
+                            <th>Product</th>
+                            <th>Category</th>
+                            <th>Destination</th>
+                            <th>Quantity</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        {
+                        this.state.isLoaded ?
+                        <>{this.state.requestList.map(function (request, index) {
+
+
+                        //  <Link to={{pathname: `/requests/${this.state.requestList[index]._id}`}} >
+                        return <tr>
+                                <td>{request.name}</td>
+                                <td>{request.category}</td>
+                                <td>{request.destination}</td>
+                                <td>{request.quantity}</td>
+                               </tr>
+                        // </Link>
+
+
+                        }, this)}</>
+                        :
+                        <p>Not Loaded</p>
+                        }
+
+                        </tbody>
+                    </Table>
+                    </>
+
         );
-     }
+        
     }
 }
 
